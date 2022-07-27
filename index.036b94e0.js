e597c283ab32b6a75a74c89f3d67e613a6b9461e
PelageyaTelezhynska <105298126+PelageyaTelezhynska@users.noreply.github.com>
2022-07-27 16:45:12 +0000

    Deploying to gh-pages from @ PelageyaTelezhynska/command17-project@5735d3d123884ae5f79277bb5aab8b8c367877cb 🚀

--- index.036b94e0.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-contacts__modal-open]"),closeModalBtn:document.querySelector("[data-contacts__modal-close]"),modal:document.querySelector("[data-contacts__modal-map]")};function t(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",t),o.closeModalBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.036b94e0.js.map
